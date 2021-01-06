85f8426392b13477a05443616bd1ddf68471d209
RyanDBarnett <conner2468@gmail.com>
2021-01-06 14:47:29 -0700

    Update github info api file

--- lib/github_info.rb
+++ lib/github_info.rb
@@ -2,15 +2,32 @@ require 'faraday'
 require 'json'
 require 'pry'
 
-response = Faraday.get("https://api.github.com/users/foymikek/repos")
-parsed_repos = JSON.parse(response.body, symbolize_names: true)
-esty_repo = parsed_repos.find do |repo|
-  repo[:name] === 'little-esty-shop'
+def get_and_parse(api_path)
+  response = Faraday.get(api_path)
+  JSON.parse(response.body, symbolize_names: true)
 end
 
-#Repo Names
-repo_name = esty_repo[:name]
+def find_repo(repos, target_repo_name)
+  repos.find do |repo|
+    repo[:name] == target_repo_name
+  end
+end
 
-# User Names
-response = Faraday.get("https://api.github.com/repos/foymikek/little-esty-shop/collaborators")
-binding.pry
+def get_repo_name
+  parsed_repos = get_and_parse("https://api.github.com/users/foymikek/repos")
+  esty_repo = find_repo(parsed_repos, 'little-esty-shop')
+  esty_repo[:name]
+end
+
+def get_repo_contributor_usernames
+  contributors = get_and_parse("https://api.github.com/repos/foymikek/little-esty-shop/contributors")
+  contributors.map do |contributor|
+    contributor[:login]
+  end
+end
+
+#Repo Name
+esty_repo_name = get_repo_name()
+
+# Contributor User Names
+contributor_usernames = get_repo_contributor_usernames()
